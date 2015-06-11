841443b833b0c21da0ee71a99595a169d863e2bf
Robert Krahn <robert.krahn@gmail.com>
2015-06-10 18:01:04 -0700

    logging federation actions

--- federation.js
+++ federation.js
@@ -2,6 +2,7 @@ var lang      = require("lively.lang");
 var messaging = require("./messaging");
 var server    = require("./server");
 var client    = require("./client");
+var logger    = require("./logger");
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
@@ -11,6 +12,7 @@ function connect(tracker, opts, thenDo) {
   opts.isFederationConnection = true;
   opts.id = tracker.id;
 
+  logger.log("federation connect", tracker, "init connecting to %s", opts.url || opts.port);
   lang.fun.composeAsync(
     function(n) { client.start(opts, n); },
     function(client, n) {
@@ -21,6 +23,7 @@ function connect(tracker, opts, thenDo) {
       n(null, client);
     },
     function(client, n) {
+      logger.log("federation connect", tracker, "connected to %s (via %s)", opts.url || opts.port, client.id);
       tracker.ownedServerSessions[client.trackerId] = client;
       client.on("close", function() { delete tracker.ownedServerSessions[client.trackerId]; });
       n(null, client);
